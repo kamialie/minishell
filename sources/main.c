6136cce755b678734a7ff93abd3684a61b850321
yoko <kamilaliev@hotmail.com>
2019-03-19 00:28:13 +0300

    added exit command

--- sources/main.c
+++ sources/main.c
@@ -17,12 +17,15 @@ int	main(void)
 	int		ret;
 	char	buf[BUFF_SIZE + 1];
 
-	ft_putendl("working!");
+	ft_putstr(O_YELLOW "minishell " O_NC);
 	while ((ret = read(0, buf, BUFF_SIZE)))
 	{
 		buf[ret] = '\0';
-		ft_putstr(buf);
-		ft_putstr(O_YELLOW "minishell " O_NC);
+		//ft_putstr(buf);
+		if (ft_strequ(buf, "exit\n"))
+			return (0);
+		else
+			ft_putstr(O_YELLOW "minishell " O_NC);
 	}
 	return (0);
 }
