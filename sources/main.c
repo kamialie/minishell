400c5080050604c1def76aa6b15d1c5dc0a8563b
yoko <kamilaliev@hotmail.com>
2019-03-20 01:51:45 +0300

    minor fixes, working version of exit and error message and return prompt - also decided to move along with trim function call

--- sources/main.c
+++ sources/main.c
@@ -12,14 +12,6 @@
 
 #include "minishell.h"
 
-void	not_found(char *str)
-{
-	ft_putstr(N_FOUND);
-	while (*str != '\0' && *str != '\n' && *str != '\t' && *str != ' ')
-		write(1, str++, 1);
-	ft_putstr("\n" O_YELLOW "minishell " O_NC);
-}
-
 int	check_command(char *str)
 {
 	int		i;
@@ -27,16 +19,14 @@ int	check_command(char *str)
 	char	**command;
 
 	i = -1;
+	value = 0;
 	command = ft_strsplit(str, ' ');
-	free(str);
 	if (ft_strequ(command[0], "exit"))
 		value = 1;
-	else if (ft_strequ(command[0], "echo"))
-		value = 2;
 	else
 	{
-		ft_putstr(command[0]);
-		value = 0;
+		ft_putstr(N_FOUND);
+		ft_putendl(command[0]);
 	}
 	while (command[++i] != NULL)
 		free(command[i]);
@@ -46,8 +36,6 @@ int	check_command(char *str)
 
 int	main(void)
 {
-	int		i;
-	int		value;
 	int		ret;
 	char	buf[BUFF_SIZE + 1];
 	char	*str;
@@ -55,21 +43,11 @@ int	main(void)
 	ft_putstr(O_YELLOW "minishell " O_NC);
 	while ((ret = read(0, buf, BUFF_SIZE)))
 	{
-		i = 0;
 		buf[ret] = '\0';
-		while (buf[i] != '\0' && buf[i] == '\t' && buf[i] == ' ' && buf[i] == '\n')
-			i++;
-		if (buf[i] != '\0')
-		{
-			str = ft_strtrim(buf);
-			if ((value = check_command(str)))
-			{
-				if (value == 1)
-					return (0);
-			}
-			else
-				ft_putstr(N_FOUND);
-		}
+		str = ft_strtrim(buf);
+		if (*str != '\0' && check_command(str))
+			return (0);
+		free(str);
 		ft_putstr(O_YELLOW "minishell " O_NC);
 	}
 	return (0);
