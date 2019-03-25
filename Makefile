3a939f698cbde3ba34daa6318f7d681262e21147
Rachael Gyles <rgyles@e2r1p5.21-school.ru>
2019-03-25 19:51:08 +0300

    outsourced env.c

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: lwyl-the <lwyl-the@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/02/20 10:25:27 by rgyles            #+#    #+#              #
-#    Updated: 2019/03/25 15:24:56 by rgyles           ###   ########.fr        #
+#    Updated: 2019/03/25 18:47:37 by rgyles           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -27,6 +27,7 @@ INCLUDES = -I includes -I libft
 SRC = main.c\
 	  init_environment.c\
 	  commands.c\
+	  env.c\
 	  echo.c
 
 OBJ = $(addprefix $(OBJ_DIR)/, $(SRC:.c=.o))
--- sources/env.c
+++ sources/env.c
@@ -1,29 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   commands.c                                         :+:      :+:    :+:   */
+/*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rgyles <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/03/25 15:23:27 by rgyles            #+#    #+#             */
-/*   Updated: 2019/03/25 18:47:40 by rgyles           ###   ########.fr       */
+/*   Created: 2019/03/25 18:47:05 by rgyles            #+#    #+#             */
+/*   Updated: 2019/03/25 18:47:26 by rgyles           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	command(char **commands, char **my_env)
+void	env(char *str, char **my_env)
 {
-	pid_t	childPID;
-
-	childPID = fork();
-	if (childPID >= 0)
+	if (str != NULL)
 	{
-		if (childPID == 0)
-			execve("/bin/pwd", commands, my_env);
-		else
-			wait(NULL);
+		ft_putstr("env: ");
+		ft_putstr(str);
+		ft_putendl(": No such file or directory");
 	}
 	else
-		ft_putendl("fork failed");
+	{
+		while (*my_env != NULL)
+		{
+			ft_putendl(*my_env);
+			++my_env;
+		}
+	}
 }
